c2cda5662993eaeeb9bc00b495ffeb8e45468513
Dwane Richards <drichards@pagerduty.com>
2023-02-07 12:17:39 -0500

    comment out logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
 
 <div class="section-center">
-			<img src={require("../img/ariana.logo.png")} alt={"Ariana Weisner Logo"} style={{height: '100%'}}/>
+			{/* <img src={require("../img/ariana.logo.png")} alt={"Ariana Weisner Logo"} style={{height: '100%'}}/> */}
   		<h1 class="mb-0">HOME</h1>
   	</div>
 )}
