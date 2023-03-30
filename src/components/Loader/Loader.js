95e5a3b0a24283ceea57cec0ab7529787fcc5628
emmanuelobi <obiemmy123@gmail.com>
2023-03-30 21:40:11 +0100

    finished loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import motion from 'framer-motion';
+import React, { useEffect } from 'react';
 import classes from './Loader.module.css';
 import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Loader = () => {
   const navigate = useNavigate();
-  const line1 = '"Be The Best Dressed in the Room';
-  const line2 = 'By';
-  const line3 = 'Ariana Weisner"';
+  const line1 = '"Be The Best Dressed In The Room"';
+  const line2 = '- Ariana Weisner';
 
   const sentence = {
-    hidden: { opacity: 1 },
+    hidden: { opacity: 1, y: 100, fontFamily: 'Antic Didone' },
     visible: {
       opacity: 1,
+      y: 0,
       transition: {
+        duration: 3,
         delay: 0.5,
-        staggerChildren: 0.08,
+        staggerChildren: 0.12,
       },
     },
   };
@@ -48,7 +49,12 @@ const Loader = () => {
   }, []);
   return (
     <div className={classes.pageBody}>
-      {/* <motion.h3 variants={sentence} initial="hidden" animate="visible">
+      <motion.h3
+        className={classes.headingText}
+        variants={sentence}
+        initial="hidden"
+        animate="visible"
+      >
         {line1.split('').map((char, index) => {
           return (
             <motion.span key={char + '-' + index} variants={letter}>
@@ -56,6 +62,7 @@ const Loader = () => {
             </motion.span>
           );
         })}
+        <br />
         {line2.split('').map((char, index) => {
           return (
             <motion.span key={char + '-' + index} variants={letter}>
@@ -63,14 +70,7 @@ const Loader = () => {
             </motion.span>
           );
         })}
-        {line3.split('').map((char, index) => {
-          return (
-            <motion.span key={char + '-' + index} variants={letter}>
-              {char}
-            </motion.span>
-          );
-        })}
-      </motion.h3> */}
+      </motion.h3>
     </div>
   );
 };
