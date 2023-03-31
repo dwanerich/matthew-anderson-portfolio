27e10baa81354a811e4a8efe29e7111e7fcbb59f
Dwane Richards <drichards@pagerduty.com>
2023-03-31 17:48:32 -0400

    home div color revisions

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -38,29 +38,6 @@ export const Brands = () => {
     <li>
         <img src="https://assets.stickpng.com/images/61487f2fd329bb0004dbd33a.png" className="pics" alt="" />
     </li>
-
-    <li>
-        <img src="https://sarahstaceydesign.com/wp-content/uploads/2015/05/Architectural-Digest-Logo.jpg" className="pics" alt="" />
-    </li>
-    <li>
-        <img src="https://www.logolynx.com/images/logolynx/90/9021a255c9359d64ca8c0caae261858e.jpeg" className="pics" alt="" />
-    </li>
-
-    <li>
-        <img src="https://assets.stickpng.com/images/61487f2fd329bb0004dbd33a.png" className="pics" alt="" />
-    </li>
-
-    <li>
-        <img src="https://sarahstaceydesign.com/wp-content/uploads/2015/05/Architectural-Digest-Logo.jpg" className="pics" alt="" />
-    </li>
-    <li>
-        <img src="https://www.logolynx.com/images/logolynx/90/9021a255c9359d64ca8c0caae261858e.jpeg" className="pics" alt="" />
-    </li>
-
-    <li>
-        <img src="https://assets.stickpng.com/images/61487f2fd329bb0004dbd33a.png" className="pics" alt="" />
-    </li>
-
 </ul>
 </div>
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -12,34 +12,38 @@ const Home = () => {
           <img
             src={require('../img/tagline.png')}
             alt={'Ariana Weisner'}
-            style={{ width: '100%' }}
+            style={{ width: '85%', borderRadius: '40px' }}
           />
         </div>{' '}
         <div className="box">
           <img
             src={require('../img/styling.png')}
             alt={'Ariana Weisner'}
-            style={{ width: '95%', borderRadius: '20px' }}
+            style={{ width: '90%', borderRadius: '40px' }}
           />
         </div>{' '}
+        <br />
         <div className="box">
           <img
             src={require('../img/ps.png')}
             alt={'Ariana Weisner'}
-            style={{ width: '95%', borderRadius: '20px' }}
+            style={{ width: '85%', borderRadius: '40px' }}
           />
         </div>
+        <br />
         <div className="box">
           <img
             src={require('../img/closet.png')}
             alt={'Ariana Weisner'}
-            style={{ width: '95%', borderRadius: '20px' }}
+            style={{ width: '95%', borderRadius: '40px' }}
           />
         </div>
+        <br />
       </div>
 
       <div class="mb-0">
         <Brands />
+        <br />
       </div>
     </center>
   );
